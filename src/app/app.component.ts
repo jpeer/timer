67965ec441ad54509effaac879730c4a4ec179db
jpeer <joachim.peer@gmail.com>
2016-12-15 19:33:19 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
+import {Observable, Subscription} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works2!';
+  lastStart : number;
+  running : boolean;
+  elapsed : number;
+  timer : Subscription;
+
+  updateElapsed() {
+    this.elapsed = new Date().getTime() - this.lastStart;
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  onKeydown(ev: KeyboardEvent) : void {
+    console.log("yep!")
+
+    if(!this.running) {
+      this.running = true;
+      this.lastStart = new Date().getTime();
+      this.timer = Observable.timer(0, 100).subscribe(n => this.updateElapsed());
+
+    } else {
+      this.running = false;
+      this.timer.unsubscribe();
+    }
+
+
+  }
+
 }
